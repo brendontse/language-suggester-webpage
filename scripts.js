a5a6b4f0be2f21e36d923e4c6075fe317fbde79c
Brendon Tse <brendonjtse@gmail.com>
2019-08-16 16:48:07 -0700

    debug and dry up code in scripts.js

--- scripts.js
+++ scripts.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
   $('form.questions').submit(function(event){
     event.preventDefault();
 
-    var pet = parseInt($('.questions#pet').val());
-    var lifestyle = parseInt($('.questions#lifestyle').val());
-    var schedule = parseInt($('.questions#schedule')).val();
-    var social = parseInt($('.questions#social').val());
-    var money = parseInt($('.questions#money').val());
-    var score = pet + lifestyle + schedule + social + money;
-
+    var pet = $('.responses#pet').val();
+    var lifestyle = $('.responses#lifestyle').val();
+    var schedule = $('.responses#schedule').val();
+    var social = $('.responses#social').val();
+    var money = $('.responses#money').val();
+    var score = parseInt(pet) + parseInt(lifestyle) + parseInt(schedule) + parseInt(social) + parseInt(money);
     console.log(pet);
     console.log(lifestyle);
     console.log(schedule);
