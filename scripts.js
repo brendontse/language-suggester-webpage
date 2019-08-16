b28e7e966955444f2b8f7aa20b160deb2622d423
Brendon Tse <brendonjtse@gmail.com>
2019-08-16 16:43:51 -0700

    define variables using user data from form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  
+  $('form.questions').submit(function(event){
+    event.preventDefault();
+
+    var pet = parseInt($('.questions#pet').val());
+    var lifestyle = parseInt($('.questions#lifestyle').val());
+    var schedule = parseInt($('.questions#schedule')).val();
+    var social = parseInt($('.questions#social').val());
+    var money = parseInt($('.questions#money').val());
+    var score = pet + lifestyle + schedule + social + money;
+
+    console.log(pet);
+    console.log(lifestyle);
+    console.log(schedule);
+    console.log(social);
+    console.log(money);
+    console.log(score);
+
+  })
 })
